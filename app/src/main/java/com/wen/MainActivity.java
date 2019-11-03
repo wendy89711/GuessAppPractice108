ff6b29678d69eb00e2e9aa7a88f42dbaff9c5ccb
wupeiwen <wendy89711@gmail.com>
2019-11-04 00:33:21 +0800

    完成！很多地方都不太熟

--- app/src/main/java/com/wen/MainActivity.java
+++ app/src/main/java/com/wen/MainActivity.java
@@ -1,36 +1,76 @@
 package com.wen;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.tabs.TabLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-
+    int secret = new Random().nextInt(10)+1;//不熟
+    int counter;
+    private TextView number;
+    private ImageView result;
+    private TextView edCounter;
+    String TAG = MainActivity.class.getSimpleName();//超不熟
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        Log.d(TAG,"secret:" + secret);//超不熟
+        number = findViewById(R.id.ed_number);
+        result = findViewById(R.id.result);
+        edCounter = findViewById(R.id.view_counter);
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                reset();
             }
         });
     }
-
+    public void reset() {
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+    }
+    public void butten(View view) {
+        int guess = Integer.parseInt(number.getText().toString());//不熟
+        counter++;
+        edCounter.setText(counter + "");
+        result.setVisibility(View.VISIBLE);
+        result.setAlpha(1.0f);
+        if (guess == secret) {
+            result.setImageResource(R.drawable.right);
+            Toast.makeText(MainActivity.this,"Bingo",Toast.LENGTH_LONG)
+                    .show();
+        } else if (guess > secret) {
+            result.setImageResource(R.drawable.error);
+            result.animate().alpha(0.0f).setDuration(1200);
+            Toast.makeText(MainActivity.this,"Lower", Toast.LENGTH_LONG)
+                    .show();
+        } else if (guess < secret) {
+            result.setImageResource(R.drawable.error);
+            result.animate().alpha(0.0f).setDuration(1200);
+            Toast.makeText(MainActivity.this,"Higher", Toast.LENGTH_LONG)
+                    .show();
+        }
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
