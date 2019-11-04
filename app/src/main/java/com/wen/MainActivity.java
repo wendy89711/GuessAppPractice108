1ee199d6b4232141f5f76b1d8308d5ce93356535
wupeiwen <wendy89711@gmail.com>
2019-11-05 00:02:42 +0800

    重新打好了

--- app/src/main/java/com/wen/MainActivity.java
+++ app/src/main/java/com/wen/MainActivity.java
@@ -21,24 +21,57 @@ import android.widget.Toast;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-
+    int secret = new Random().nextInt(10)+1;
+    int counter;
+    private TextView number;
+    private TextView edcounter;
+    private ImageView result;
+    String TAG = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        Log.d(TAG,"secret:" + secret);
+        number = findViewById(R.id.ed_number);
+        edcounter = findViewById(R.id.view_counter);
+        result = findViewById(R.id.result);
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                reset();
             }
         });
     }
 
-
+    public void reset() {
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+    }
+    public void button(View view) {
+        int n = Integer.parseInt(number.getText().toString());
+        counter++;
+        edcounter.setText(counter + "");
+        result.setVisibility(View.VISIBLE);
+        result.setAlpha(1.0f);
+        if(n == secret) {
+            result.setImageResource(R.drawable.right);
+            Toast.makeText(MainActivity.this,"Bingo",Toast.LENGTH_LONG)
+                    .show();
+        } else if (n > secret) {
+            result.setImageResource(R.drawable.error);
+            result.animate().alpha(0.0f).setDuration(1200);
+            Toast.makeText(MainActivity.this,"Lower",Toast.LENGTH_LONG)
+                    .show();
+        } else if (n < secret) {
+            result.setImageResource(R.drawable.error);
+            result.animate().alpha(0.0f).setDuration(1200);
+            Toast.makeText(MainActivity.this,"Higher",Toast.LENGTH_LONG)
+                    .show();
+        }
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
